8b6d0966ab80183ea10401af13ad5332a2119b53
endrin <158015+endrin@users.noreply.github.com>
2025-02-02 22:33:53 +0200

    Add splitting by links and images

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -2,6 +2,9 @@ import re
 
 from textnode import TextNode, TextType
 
+_IMAGE_RE_ = re.compile(r"!\[([^\]]+)\]\(([^\)]+)\)")
+_LINK_RE_ = re.compile(r"(?<!!)\[([^\]]+)\]\(([^\)]+)\)")
+
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     def split_node(node: TextNode):
@@ -17,10 +20,47 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
 
 def extract_markdown_images(text: str) -> list[tuple[str, str]]:
-    image_re = re.compile(r"!\[([^\]]+)\]\(([^\)]+)\)")
-    return image_re.findall(text)
+    return _IMAGE_RE_.findall(text)
 
 
 def extract_markdown_links(text: str) -> list[tuple[str, str]]:
-    link_re = re.compile(r"(?<!!)\[([^\]]+)\]\(([^\)]+)\)")
-    return link_re.findall(text)
+    return _LINK_RE_.findall(text)
+
+
+def split_by_pattern(text, pattern):
+    indices = [0, *(pos for m in pattern.finditer(text) for pos in m.span()), None]
+    return [text[i:j] for i, j in zip(indices, indices[1:])]
+
+
+def split_nodes_image(old_nodes):
+    def do_split(node):
+        for chunk in split_by_pattern(node.text, _IMAGE_RE_):
+            if m := _IMAGE_RE_.fullmatch(chunk):
+                alt_text, url = m.group(1, 2)
+                yield TextNode(alt_text, TextType.IMAGE, url)
+            else:
+                yield TextNode(chunk, TextType.TEXT)
+
+    return [
+        new_node
+        for old_node in old_nodes
+        for new_node in do_split(old_node)
+        if new_node.text != ""
+    ]
+
+
+def split_nodes_link(old_nodes):
+    def do_split(node):
+        for chunk in split_by_pattern(node.text, _LINK_RE_):
+            if m := _LINK_RE_.fullmatch(chunk):
+                alt_text, url = m.group(1, 2)
+                yield TextNode(alt_text, TextType.LINK, url)
+            else:
+                yield TextNode(chunk, TextType.TEXT)
+
+    return [
+        new_node
+        for old_node in old_nodes
+        for new_node in do_split(old_node)
+        if new_node.text != ""
+    ]
--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -4,6 +4,8 @@ from split_nodes import (
     extract_markdown_images,
     extract_markdown_links,
     split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
 )
 from textnode import TextNode, TextType
 
@@ -35,6 +37,41 @@ class TestSplitNodes(unittest.TestCase):
             TextNode(" title", TextType.TEXT),
         ]
 
+        self.assertEqual(new_nodes, expected_nodes)
+
+    def test_split_nodes_link(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        expected_nodes = [
+            TextNode("This is text with a link ", TextType.TEXT),
+            TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode(
+                "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+            ),
+        ]
+        self.assertEqual(new_nodes, expected_nodes)
+
+    def test_split_nodes_image(self):
+        node = TextNode(
+            (
+                "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) "
+                "and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg) "
+                "and some text at the end"
+            ),
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        expected_nodes = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("rick roll", TextType.IMAGE, "https://i.imgur.com/aKaOqIh.gif"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("obi wan", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and some text at the end", TextType.TEXT),
+        ]
         self.assertEqual(new_nodes, expected_nodes)
 
 
