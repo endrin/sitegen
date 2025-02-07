8b7f3a19b0d5ea6fe31bf79f8b954447f844e55b
endrin <158015+endrin@users.noreply.github.com>
2025-02-08 00:44:55 +0200

    Implement mirroring of static assets

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def mirror(source: str, destination: str, *, verbose=False):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.makedirs(destination, exist_ok=True)
+
+    for entry in os.scandir(source):
+        src_path = entry.path
+        dst_path = src_path.replace(source, destination)
+
+        if entry.is_dir():
+            mirror(src_path, dst_path, verbose=verbose)
+            continue
+
+        if verbose:
+            print(f"{src_path} -> {dst_path}")
+            shutil.copy2(src_path, dst_path)
 
 
 def main():
-    print(TextNode("texnode test", TextType.IMAGE, "https://www.boot.dev"))
+    mirror("static", "public", verbose=True)
 
 
 if __name__ == "__main__":
